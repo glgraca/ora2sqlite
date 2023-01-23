221a4a2b6be714f1178fde798bfd4a44ba0a0426
glgraca <glgraca@tce.rs.gov.br>
2023-01-23 16:58:00 -0300

    Handles BLOBs, CLOBs, XMLType and LONGs

--- README.md
+++ README.md
@@ -15,8 +15,9 @@ Use: ora2sqlite -s oracle -u username -p password
   -d --destination SQLite filename (defaults to the oracle schema name)
   -v --views Copy views
   -V --view-filter View filter
-  -b --blobs Copy blobs 
-  -c --clobs Copy clobs (LONG is treated as CLOB)
+  -b --blobs Copy blobs (RAW is treated as BLOB) 
+  -c --clobs Copy clobs
+  -x --xml Copy XML (XMLTYPE is treated as text)
   -f --table-filter Filter tables by name
   -r --rows Max number of rows
   -I --indices Copy indices
@@ -24,15 +25,26 @@ Use: ora2sqlite -s oracle -u username -p password
   -P --pks Copy primary keys
   -A Copy indices, fks, and pks (same as -PFI)
 
-  Example: ora2sqlite -s server:1521/service -u data -p pass -f "table_name in ('TEST')" -r 100
+  LONGs cannot be retrieved, so they are always set to null.
+
+  Examples: 
+  
+    # Copy 100 rows of table TEST from DATA schema
+    ora2sqlite -s server:1521/service -u data -p pass -f "table_name in ('TEST')" -r 100
+
+    # Copy all tables of DATA with 100 rows, primary keys, foreign keys, indices, clobs, blobs, and XMLType
+    ora2sqlite -s server:1521/service -u data -p pass -r 100 -A -cbx
+
+    # Select some views
+    ora2sqlite -s server:1521/service -u data -p pass -v -V "view_name like '%HR%'"
 ```
 
 TODO:
 
-- Handle BLOBS, CLOBS, etc
+- ~~Handle BLOBS, CLOBS, etc~~
 - Test dates and timestamps
 - Verify BFILEs (replace by filename)
 - ~~Print some indication of progress~~
 - ~~Optimise inserts~~
 - ~~Filter out views and give option to include them (probably as tables, not views - also requires new filter)~~
-- Add examples to the documentation
+- ~~Add examples to the documentation~~
--- ora2sqlite.pl
+++ ora2sqlite.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 use Data::Dumper;
-use DBI;
+use DBI qw(:sql_types);
 use DBD::Oracle;
 use DBD::SQLite;
 use Getopt::Long qw(:config no_ignore_case bundling);
@@ -8,118 +8,122 @@ use open ':std', ':encoding(utf8)';
 use utf8;
 use strict;
 
-my $banner=<<EOD;
-Use: ora2sqlite -s oracle -u username -p password [-d sqlite] [-b] [-c] 
+{
+  my $banner=<<EOD;
+  Use: ora2sqlite -s oracle -u username -p password [-d sqlite] [-b] [-c] 
 
-  -s --source Oracle Database 
-  -u --username Oracle schema 
-  -p --password Oracle password 
-  -d --destination SQLite filename (defaults to the oracle schema name)
-  -v --views Copy views
-  -V --view-filter View filter
-  -b --blobs Copy blobs 
-  -c --clobs Copy clobs (LONG is treated as CLOB)
-  -f --table-filter Filter tables by name
-  -r --rows Max number of rows
-  -I --indices Copy indices
-  -F --fks Copy foreign keys
-  -P --pks Copy primary keys
-  -A Copy indices, fks, and pks (same as -PFI)
+    -s --source Oracle Database 
+    -u --username Oracle schema 
+    -p --password Oracle password 
+    -d --destination SQLite filename (defaults to the oracle schema name)
+    -v --views Copy views
+    -V --view-filter View filter
+    -b --blobs Copy blobs (RAW is treated as BLOB)
+    -c --clobs Copy clobs
+    -x --xml Copy XML (XMLTYPE is treated as text)
+    -f --table-filter Filter tables by name
+    -r --rows Max number of rows
+    -I --indices Copy indices
+    -F --fks Copy foreign keys
+    -P --pks Copy primary keys
+    -A Copy indices, fks, and pks (same as -PFI)
 
-  Example: ora2sqlite -s server:1521/service -u data -p pass -d data.db -f "table_name in ('TEST')" -r 100
+    LONGs cannot be retrieved, so they are always set to null.
+
+    Example: ora2sqlite -s server:1521/service -u data -p pass -d data.db -f "table_name in ('TEST')" -r 100
 EOD
 
-my ($oracle_database, $oracle_username, $oracle_password);
-my ($sqlite_filename, $copy_views, $view_name_filter, $copy_blobs, $copy_clobs);
-my $table_name_filter;
-my $max_rows;
-my ($copy_indices, $copy_foreign_keys, $copy_primary_keys, $copy_all_constraints);
+  my ($oracle_database, $oracle_username, $oracle_password, $sqlite_filename, 
+      $copy_views, $view_name_filter, $table_name_filter, $max_rows,
+      $copy_foreign_keys, $copy_primary_keys, $copy_all_constraints,
+      $copy_blobs, $copy_clobs, $copy_xml, $copy_indices);
 
-my $options={};
+  GetOptions(
+    's|source=s'=>\$oracle_database,
+    'u|username=s'=>\$oracle_username,
+    'p|password=s'=>\$oracle_password,
+    'd|destination=s'=>\$sqlite_filename,
+    'v|views'=>\$copy_views,
+    'V|view-filter=s'=>\$view_name_filter,
+    'b|blobs'=>\$copy_blobs,
+    'c|clobs'=>\$copy_clobs,
+    'x|xml'=>\$copy_xml,
+    'f|table-filter=s'=>\$table_name_filter,
+    'r|rows=s'=>\$max_rows,
+    'I|indices'=>\$copy_indices,
+    'F|fks'=>\$copy_foreign_keys,
+    'P|pks'=>\$copy_primary_keys,
+    'A'=>\$copy_all_constraints
+  ) or die $banner;
 
-GetOptions(
-  's|source=s'=>\$oracle_database,
-  'u|username=s'=>\$oracle_username,
-  'p|password=s'=>\$oracle_password,
-  'd|destination=s'=>\$sqlite_filename,
-  'v|views'=>\$copy_views,
-  'V|view-filter=s'=>\$view_name_filter,
-  'b|blobs'=>\$copy_blobs,
-  'c|clobs'=>\$copy_clobs,
-  'f|table-filter=s'=>\$table_name_filter,
-  'r|rows=s'=>\$max_rows,
-  'I|indices'=>\$copy_indices,
-  'F|fks'=>\$copy_foreign_keys,
-  'P|pks'=>\$copy_primary_keys,
-  'A'=>\$copy_all_constraints
-) or die $banner;
+  die $banner unless defined $oracle_database && defined $oracle_username && defined $oracle_password;
 
-die $banner unless defined $oracle_database && defined $oracle_username && defined $oracle_password;
+  $sqlite_filename="${oracle_username}.db" if !defined $sqlite_filename;
 
-$sqlite_filename="${oracle_username}.db" if !defined $sqlite_filename;
+  $copy_indices=1, $copy_foreign_keys=1, $copy_primary_keys=1 if $copy_all_constraints;
 
-$copy_indices=1, $copy_foreign_keys=1, $copy_primary_keys=1 if $copy_all_constraints;
+  $view_name_filter='1=2' if !$copy_views;
 
-$view_name_filter='1=2' if !$copy_views;
+  unlink $sqlite_filename;
 
-unlink $sqlite_filename;
+  my $oracle=DBI->connect("dbi:Oracle://$oracle_database", $oracle_username, $oracle_password, {ReadOnly=>1, LongReadLen=>1024*1024*1024, LongTruncOk=>1});
+  $oracle->do(q(alter session set nls_timestamp_format = 'YYYY-MM-DD"T"HH24:MI:SS.ff3"Z"'));
+  $oracle->do(q(alter session set nls_date_format = 'YYYY-MM-DD"T"HH24:MI:SS'));
 
-my $oracle=DBI->connect("dbi:Oracle://$oracle_database", $oracle_username, $oracle_password, {ReadOnly=>1, LongReadLen=>100*1024*1024});
-$oracle->do(q(alter session set nls_timestamp_format = 'YYYY-MM-DD"T"HH24:MI:SS.ff3"Z"'));
-$oracle->do(q(alter session set nls_date_format = 'YYYY-MM-DD"T"HH24:MI:SS'));
+  my $sqlite=DBI->connect("dbi:SQLite:dbname=$sqlite_filename",'','');
+  $sqlite->{sqlite_unicode} = 1;
 
-my $sqlite=DBI->connect("dbi:SQLite:dbname=$sqlite_filename",'','');
-$sqlite->{sqlite_unicode} = 1;
+  my $tables=get_oracle_tables($oracle, $table_name_filter, $view_name_filter);
+  create_sqlite_tables($oracle, $sqlite, $tables, $copy_primary_keys, $copy_foreign_keys);
+  copy_data($oracle, $sqlite, $tables, $copy_blobs, $copy_clobs, $copy_xml, $max_rows);
+  create_sqlite_indices($oracle, $sqlite, $table_name_filter, $view_name_filter) if $copy_indices;
 
-my $tables=get_oracle_tables($oracle, $table_name_filter, $view_name_filter);
-create_sqlite_tables($sqlite, $tables);
-copy_data($oracle, $sqlite, $tables, $max_rows);
-create_sqlite_indices($oracle, $sqlite, $table_name_filter, $view_name_filter) if $copy_indices;
+  # { table => [ { id=>123, name=>column_name, type=>data_type, nullable=>'Y|N', oracle_type=>oracle_data_type } ]... }
+  sub get_oracle_tables {
+    my ($oracle, $filter, $view_filter)=@_;
+    my $tables={};
 
-# { table => [ { name=>column_name, type=>column_type } ]... }
-sub get_oracle_tables {
-  my $oracle=shift;
-  my $filter=shift;
-  my $view_filter=shift;
-  my $tables={};
+    $filter="where $filter" if $filter;
+    $view_filter="where $view_filter" if $view_filter;
 
-  $filter="where $filter" if $filter;
-  $view_filter="where $view_filter" if $view_filter;
+    my $query=qq(
+      select lower(table_name), 
+        row_number() over (partition by table_name order by column_id) column_id,
+        lower(column_name),
+        case 
+          when data_type like 'TIMESTAMP%' then 'DATETIME'
+          when data_type like 'DATE' then 'DATETIME'
+          when data_type='NUMBER' and data_scale=0 then 'INTEGER'
+          when data_type in ('FLOAT', 'NUMBER') then 'REAL'
+          when data_type in ('CHAR', 'NCHAR', 'VARCHAR2', 'NVARCHAR2') then 'TEXT'
+          when data_type in ('RAW', 'BLOB') then 'BLOB'
+          when data_type in ('CLOB', 'LONG') then 'TEXT'
+          when data_type='XMLTYPE' then 'TEXT'
+          else 'TEXT'
+          end data_type,
+          nullable,
+          data_type oracle_data_type
+      from user_tab_cols       
+      where hidden_column='NO'
+            and (table_name in (select table_name from user_tables $filter) 
+                or table_name in (select view_name from user_views $view_filter))
+      order by table_name, column_id
+    );
 
-  my $query=qq(
-    select lower(table_name), 
-       lower(column_name),
-       case 
-         when data_type like 'TIMESTAMP%' then 'DATETIME'
-         when data_type like 'DATE' then 'DATETIME'
-         when data_type='NUMBER' and data_scale=0 then 'INTEGER'
-         when data_type in ('FLOAT', 'NUMBER') then 'REAL'
-         when data_type in ('CHAR', 'NCHAR', 'VARCHAR2', 'NVARCHAR2') then 'TEXT'
-         when data_type='BLOB' then 'BLOB'
-         when data_type in ('CLOB', 'LONG', 'XMLTYPE') then 'TEXT'
-         else 'TEXT'
-         end data_type,
-         nullable,
-         data_type oracle_data_type
-    from user_tab_cols       
-    where VIRTUAL_COLUMN='NO' 
-          and (table_name in (select table_name from user_tables $filter) or table_name in (select view_name from user_views $view_filter))
-    order by table_name, column_id
-  );
+    my $st=$oracle->prepare($query);
+    $st->execute();
+    while(my @row=$st->fetchrow_array()) {
+        my ($table_name, $column_id, $column_name, $column_type, $nullable, $oracle_type)=@row;
+        push @{$tables->{$table_name}}, {'id'=>$column_id, 'name'=>$column_name, 'type'=>$column_type, 'nullable'=>$nullable, 'oracle_type'=>$oracle_type};
+    };
 
-  my $st=$oracle->prepare($query);
-  $st->execute();
-  while(my @row=$st->fetchrow_array()) {
-      my ($table_name, $column_name, $column_type, $nullable, $oracle_type)=@row;
-      push @{$tables->{$table_name}}, {'name'=>$column_name, 'type'=>$column_type, 'nullable'=>$nullable, 'oracle_type'=>$oracle_type};
+    return $tables;
   };
 
-  return $tables;
-};
+}
 
 sub create_sqlite_tables {
-  my $sqlite=shift;
-  my $tables=shift;
+  my ($oracle, $sqlite, $tables, $copy_primary_keys, $copy_foreign_keys)=@_;
 
   print "Creating tables\n";
 
@@ -145,11 +149,8 @@ sub create_sqlite_tables {
 }
 
 sub copy_data {
-  my $oracle=shift;
-  my $sqlite=shift;
-  my $tables=shift;
-  my $max_rows=shift||'';
-
+  my ($oracle, $sqlite, $tables, $copy_blobs, $copy_clobs, $copy_xml, $max_rows)=@_;
+  
   print "Copying data\n";
 
   $max_rows="where rownum<$max_rows+1" if $max_rows;
@@ -161,12 +162,51 @@ sub copy_data {
     $sqlite->do('begin transaction');
     print "  $table_name ($current_table/$table_count)\n";
     my $columns=$tables->{$table_name};
-    my $st=$oracle->prepare("select * from $table_name $max_rows");
+    my $select_columns=join ',', map {
+      if(!$copy_blobs and $_->{type} eq 'BLOB') {
+        'null'
+      } elsif(!$copy_clobs and ($_->{oracle_type} eq 'RAW' or $_->{oracle_type} eq 'CLOB')) {
+        'null'
+      } elsif($_->{oracle_type} eq 'XMLTYPE') {
+        if($copy_xml) {
+         '('.$_->{name}.').getClobVal()'
+        } else {
+          'null'
+        }
+      } elsif($_->{oracle_type} eq 'LONG') {
+        'null'
+      } else {
+        $_->{name}
+      }
+    } @$columns;
+    my $select_cmd="select $select_columns from $table_name $max_rows";
+    my $st=$oracle->prepare($select_cmd);
     $st->execute();
     my $insert_cmd="insert into $table_name values (". (join ',', map { '?' } @$columns).')';
     my $insert_st=$sqlite->prepare($insert_cmd);
     while(my @row=$st->fetchrow_array()) {
-       $insert_st->execute(@row);
+      for my $column (@$columns) {
+        my $id=$column->{id};
+        # BLOB
+        if($column->{type} eq 'BLOB') {
+          if($copy_blobs) {
+            $insert_st->bind_param($id, $row[$id-1], SQL_BLOB);
+          } else {
+            $insert_st->bind_param($id, undef);
+          }
+        # RAW and CLOB
+        } elsif($column->{oracle_type} eq 'RAW' || $column->{oracle_type} eq 'CLOB') {
+          if($copy_clobs) {
+            $insert_st->bind_param($id, $row[$id-1]);
+          } else {
+            $insert_st->bind_param($id, undef);
+          }
+        # All other types
+        } else {
+          $insert_st->bind_param($id, $row[$id-1]);
+        }
+      }
+      $insert_st->execute();
     } 
     $sqlite->do('end transaction');
     $current_table++;
@@ -191,9 +231,7 @@ sub get_primary_key {
 }
 
 sub create_sqlite_indices {
-  my $oracle=shift;
-  my $sqlite=shift;
-  my $filter=shift;
+  my ($oracle, $sqlite, $filter)=@_;
 
   print "Creating indices\n";
 
@@ -219,8 +257,7 @@ sub create_sqlite_indices {
 }
 
 sub get_foreign_keys {
-  my $oracle=shift;
-  my $table_name=shift;
+  my ($oracle, $table_name)=@_;
 
   my $query=qq(
     select listagg(distinct a.column_name,',') within group (order by a.position), t.table_name, listagg(distinct b.column_name,',') within group (order by b.position)
